fe0223d81b648f58c4f69e3b8ce7edf387fce292
Headlessduck_ <78724847+TomSipsOnCurry@users.noreply.github.com>
2021-09-17 18:34:50 +1000

    Fixed properties of blocks

--- src/main/java/com/tomsipsoncurry/vanillaplus/registry/ModBlocks.java
+++ src/main/java/com/tomsipsoncurry/vanillaplus/registry/ModBlocks.java
@@ -3,29 +3,24 @@
     import com.tomsipsoncurry.vanillaplus.Vanillaplus;
     import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
     import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
-    import net.minecraft.block.Block;
-    import net.minecraft.block.Material;
-    import net.minecraft.block.PillarBlock;
-    import net.minecraft.block.WallBlock;
+    import net.minecraft.block.*;
     import net.minecraft.sound.BlockSoundGroup;
     import net.minecraft.util.Identifier;
     import net.minecraft.util.registry.Registry;
 
     public class ModBlocks {
         public static final Block EUCALYPTUS_LOG = new PillarBlock(FabricBlockSettings
-                .of(Material.WOOD)
-                .breakByTool(FabricToolTags.AXES)
-                .strength(2.0f, 10f)
-                .sounds(BlockSoundGroup.WOOD)
+                .copyOf(Blocks.OAK_LOG)
         );
-        public static final Block EUCALYPTUS_PLANK = new Block(FabricBlockSettings
-                .of(Material.WOOD)
-                .breakByTool(FabricToolTags.AXES)
-                .strength(2.0F, 3.0F));
-        public static final Block PURPUR_WALL = new WallBlock(FabricBlockSettings.of(Material.STONE).strength(2.0F, 6.0F));
+        public static final Block EUCALYPTUS_PLANKS = new Block(FabricBlockSettings
+                .copyOf(Blocks.OAK_PLANKS)
+        );
+        public static final Block PURPUR_WALL = new WallBlock(FabricBlockSettings
+                .copyOf(Blocks.PURPUR_BLOCK)
+        );
         public static void RegisterBlocks() {
             Registry.register(Registry.BLOCK, new Identifier(Vanillaplus.MOD_ID, "eucalyptus_log"), EUCALYPTUS_LOG);
-            Registry.register(Registry.BLOCK, new Identifier(Vanillaplus.MOD_ID, "eucalyptus_plank"), EUCALYPTUS_PLANK);
+            Registry.register(Registry.BLOCK, new Identifier(Vanillaplus.MOD_ID, "eucalyptus_plank"), EUCALYPTUS_PLANKS);
             Registry.register(Registry.BLOCK, new Identifier(Vanillaplus.MOD_ID, "purpur_wall"), PURPUR_WALL);
     }
     }
